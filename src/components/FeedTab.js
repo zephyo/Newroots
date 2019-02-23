5f4b66bf68f1d5dd180c17a497cd897dc39e67ad
Spider King <gollyzoom@gmail.com>
2019-02-22 21:10:28 -0800

    pushed correct chronological order and separation by day

--- src/components/FeedTab.js
+++ src/components/FeedTab.js
@@ -17,7 +17,7 @@ class FeedTab extends React.Component {
     this.state = {
       thoughts: [],
       checkins: [],
-
+      feed: []
     }
     //this.processTime = this.processTime.bind(this);
   }
@@ -44,51 +44,55 @@ class FeedTab extends React.Component {
     autosize($('textarea'));
     let element = this;
     let tempFeed = [];
-    let tempCheckins = this.state.checkins;
-    let tempThoughts = this.state.thoughts;
+    //let tempCheckins = this.state.checkins;
+    //let tempThoughts = this.state.thoughts;
     feedListen = this.props.firebase.user(this.props.uid).collection("feed")
       .onSnapshot((snapshot) => {
         //let tempFeed = [];
         snapshot.docChanges().forEach((change) => {
           console.log("changety[e]: " + change.type);
           if (change.type === "added") {
-            console.log("New city: ", change.doc.data());
-            tempFeed.push(change.doc.id);
-            let dat = change.doc.data();
+            //console.log("New city: ", change.doc.data());
+            //tempFeed.push(change.doc.id);
+            let data = change.doc.data();
+            let struct;
             if (change.doc.data().checkin) {
-              tempCheckins.unshift({
-                uid: dat.uid,
-                name: dat.name,
-                PpfURL: dat.PpfURL,
-                timestamp: moment(dat.timestamp).format('lll'),
+              struct = ({
+                uid: data.uid,
+                name: data.name,
+                PpfURL: data.PpfURL,
+                timestamp: moment(data.timestamp).format('lll'),
                 postid: change.doc.id,
-                checkinData: dat.checkinData
+                checkinData: data.checkinData
               })
             }
             else {
-              tempThoughts.unshift({
-                uid: dat.uid,
-                name: dat.name,
-                PpfURL: dat.PpfURL,
-                thought: dat.thought,
-                message: dat.message,
-                comments: dat.comm_cont,
+              struct = ({
+                uid: data.uid,
+                name: data.name,
+                PpfURL: data.PpfURL,
+                thought: data.thought,
+                message: data.message,
+                comments: data.comm_cont,
                 conversation: [],
                 postid: change.doc.id,
-                timestamp: moment(dat.timestamp).format('lll'),
+                timestamp: moment(data.timestamp).format('lll'),
               })
             }
+            tempFeed.push(struct);
+            console.log(struct.timestamp);
           }
 
         });
-        console.log(JSON.stringify(tempCheckins));
-        console.log(JSON.stringify(tempThoughts));
+        /*console.log(JSON.stringify(tempCheckins));
+        console.log(JSON.stringify(tempThoughts));*/
 
 
-
+        
         element.setState({
-          checkins: tempCheckins,
-          thoughts: tempThoughts
+          /*checkins: tempCheckins,
+          thoughts: tempThoughts*/
+            feed:tempFeed
         })
         /*element.setState({
           checkins: tempCheckins,
@@ -102,9 +106,9 @@ class FeedTab extends React.Component {
 
   compare = (a, b) => {
     if (a.timestamp < b.timestamp)
-      return -1;
-    if (a.timestamp > b.timestamp)
       return 1;
+    if (a.timestamp > b.timestamp)
+      return -1;
     return 0;
   }
 
@@ -112,43 +116,54 @@ class FeedTab extends React.Component {
   render() {
 
 
-    let feed = this.state.checkins.concat(this.state.thoughts);
-    feed.sort(this.compare);
+    //let feed = this.state.checkins.concat(this.state.thoughts);
+    let local_feed = this.state.feed;
+    local_feed.sort(this.compare);
+    //console.log("local_feed " + local_feed[0].timestamp);
+    let last_date = "";
+    let header = <h1 className="date-marker">February 17</h1>;
+    let feedItems = local_feed.map((f, index) => {
+        if(f.timestamp.split(",")[0] !== last_date){
+            last_date = f.timestamp.split(",")[0];
+            console.log("last date " + last_date);
+             header = <h1 className="date-marker">{last_date}</h1>;
+        }
+        else{
+             header = "";
+        }
+        if (f.checkinData) {
+            return <div>{header} <CheckinPost
+            uid={this.props.uid}
+            posterUid={f.uid}
+            
+            PpfURL={f.PpfURL}
+            yourPpfURL={this.props.PpfURL}
+            key={index.toString()+"_checkin"}
+            name={f.name}
+            timestamp={f.timestamp}
 
-    let feedItems = feed.map((f, index) => {
-      if (f.checkinData) {
-        return <CheckinPost
-          uid={this.props.uid}
-          posterUid={f.uid}
+            postid={f.postid}
+            checkinData={f.checkinData}
+            firebase={this.props.firebase}
+            /></div>;
+        } else {
+            return <div>{header} <ThoughtPost
+            uid={this.props.uid}
+            posterUid={f.uid}
+            
+            PpfURL={f.PpfURL}
+            yourPpfURL={this.props.PpfURL}
 
-          PpfURL={f.PpfURL}
-          yourPpfURL={this.props.PpfURL}
-          key={toString(index)}
-          name={f.name}
-          timestamp={f.timestamp}
+            key={index.toString()+"_thought"}
+            name={f.name}
+            thought={f.thought}
+            timestamp={f.timestamp}
 
-          postid={f.postid}
-          checkinData={f.checkinData}
-          firebase={this.props.firebase}
-        />;
-      } else {
-        return <ThoughtPost
-          uid={this.props.uid}
-          posterUid={f.uid}
-
-          PpfURL={f.PpfURL}
-          yourPpfURL={this.props.PpfURL}
-
-          key={toString(index)}
-          name={f.name}
-          thought={f.thought}
-          timestamp={f.timestamp}
-
-          message={f.message}
-          postid={f.postid}
-          firebase={this.props.firebase
-          } />;
-      }
+            message={f.message}
+            postid={f.postid}
+            firebase={this.props.firebase
+            } /></div>;
+        }
     }
     );
 
@@ -189,7 +204,7 @@ class FeedTab extends React.Component {
     // );
     // console.log(checkinItems);
     //combine checkinItems and thoughtItems then sort then render
-
+    //        <h1 className="date-marker">February 17</h1>
     return (
 
       <section className="feed">
@@ -200,7 +215,7 @@ class FeedTab extends React.Component {
           network={this.props.network}
           firebase={this.props.firebase}
         />
-        <h1 className="date-marker">February 17</h1>
+
         {feedItems}
 
 
